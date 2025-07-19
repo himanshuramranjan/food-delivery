20c8c1fcf1c35bf06cf5e92eb1c3bb6bb8ab6647
Himanshuram Ranjan <himanshuramr@gmail.com>
2025-07-19 20:40:15 +0530

    Exception added for DeliveryPerson not found

--- src/service/state/DeliverOrderCommand.java
+++ src/service/state/DeliverOrderCommand.java
@@ -1,6 +1,7 @@
 package service.state;
 
 import enums.OrderStatus;
+import exception.DeliveryPersonNotFoundException;
 import model.DeliveryPerson;
 import model.Order;
 import service.DeliveryService;
@@ -8,8 +9,11 @@ import service.DeliveryService;
 public class DeliverOrderCommand implements OrderCommand {
 
     @Override
-    public void handleOrder(Order order) {
+    public void handleOrder(Order order) throws DeliveryPersonNotFoundException {
         DeliveryPerson deliveryPerson = DeliveryService.getInstance().assignDeliveryPerson();
+
+        if(deliveryPerson == null) throw new DeliveryPersonNotFoundException("No DeliveryPerson available ");
+
         order.setDeliveryPerson(deliveryPerson);
         System.out.println("Order  is out for delivery by : " + deliveryPerson.getName());
         order.setStatus(OrderStatus.OUT_FOR_DELIVERY);
