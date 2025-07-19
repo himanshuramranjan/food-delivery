f8c50f4bd7674c79fcbf353d2f0d9bdd2573ff50
Himanshuram Ranjan <himanshuramranjan@Himanshurams-MacBook-Air.local>
2025-07-19 12:46:52 +0530

    added the functionality for placing the order

--- src/service/RestaurantService.java
+++ src/service/RestaurantService.java
@@ -1,10 +1,12 @@
 package service;
 
+import model.Order;
 import model.Restaurant;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 
 public class RestaurantService {
     private final List<Restaurant> restaurantList;
@@ -30,6 +32,11 @@ public class RestaurantService {
 
     public Restaurant getBestRestaurant() {
         return restaurantList.stream().max(Comparator.comparing(Restaurant::getAvgRating)).orElse(null);
+    }
+
+    public void placeOrder(Restaurant restaurant, Map<String, Integer> foodItems) {
+        Order order = new Order(restaurant, foodItems);
+        OrderService.getInstance().processOrder(order);
     }
 
     public List<Restaurant> getAllResturant() {
