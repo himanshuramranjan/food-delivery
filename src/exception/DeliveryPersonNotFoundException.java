eb6f6ed9224d246d5190915a51cb33a1eb6f1f2f
Himanshuram Ranjan <himanshuramranjan@Himanshurams-MacBook-Air.local>
2025-07-19 20:40:15 +0530

    Exception added for DeliveryPerson not found

---
+++ src/exception/DeliveryPersonNotFoundException.java
@@ -0,0 +1,8 @@
+package exception;
+
+public class DeliveryPersonNotFoundException extends Exception {
+
+    public DeliveryPersonNotFoundException(String message) {
+        super(message);
+    }
+}
